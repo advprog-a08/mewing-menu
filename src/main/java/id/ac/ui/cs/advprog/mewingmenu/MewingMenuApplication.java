0e52c8eed49d10905528dc90575f8cf44687383f
secona <77039267+secona@users.noreply.github.com>
2025-03-28 17:36:32 +0700

    feat: init Spring boot hello world app

---
+++ src/main/java/id/ac/ui/cs/advprog/mewingmenu/MewingMenuApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.mewingmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MewingMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MewingMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/mewingmenu/MewingMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.mewingmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MewingMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
