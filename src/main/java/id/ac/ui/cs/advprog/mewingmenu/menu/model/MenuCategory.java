a692cf695d9df0ca017b6df8cb53199d5ff2716f
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-08 16:33:09 +0800

    feat: make menu category implementation

--- src/main/java/id/ac/ui/cs/advprog/mewingmenu/menu/model/MenuCategory.java
+++ src/main/java/id/ac/ui/cs/advprog/mewingmenu/menu/model/MenuCategory.java
@@ -3,7 +3,14 @@ package id.ac.ui.cs.advprog.mewingmenu.menu.model;
 import java.util.List;
 import java.util.UUID;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,13 +25,19 @@ import lombok.ToString;
 @ToString
 public class MenuCategory {
 
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "uuid", updatable = false, nullable = false)
     private UUID id;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 5, message = "Name must be at least 5 characters")
+    @Column(nullable = false, unique = true)
     private String name;
 
-
+    @Size(max = 255, message = "Description can't exceed 255 characters")
     private String description;
 
-
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Menu> menus;
 }
