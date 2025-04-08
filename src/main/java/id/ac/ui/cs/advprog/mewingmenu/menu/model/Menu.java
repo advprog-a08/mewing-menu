43fad790d6170a1f83f94f4c83654f786cccca84
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-08 16:32:02 +0800

    feat: implement menu model

--- src/main/java/id/ac/ui/cs/advprog/mewingmenu/menu/model/Menu.java
+++ src/main/java/id/ac/ui/cs/advprog/mewingmenu/menu/model/Menu.java
@@ -3,7 +3,18 @@ package id.ac.ui.cs.advprog.mewingmenu.menu.model;
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,15 +29,31 @@ import lombok.ToString;
 @ToString
 public class Menu {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 5, message = "Name must be at least 5 characters")
+    @Column(nullable = false, unique = true)
     private String name;
 
+    @NotBlank(message = "Description is required")
+    @Size(max = 255, message = "Description can't exceed 255 characters")
+    @Column(nullable = false, length = 255)
     private String description;
 
+    @NotBlank(message = "Image URL is required")
+    @Column(nullable = false)
     private String imageUrl;
 
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = true, message = "Price must be non-negative")
+    @DecimalMax(value = "1000000.0", inclusive = true, message = "Price must be less than or equal to 1,000,000")
+    @Column(nullable = false)
     private BigDecimal price;
 
+    @ManyToOne
+    @JoinColumn(name = "category_id")
     private MenuCategory category;
 }
