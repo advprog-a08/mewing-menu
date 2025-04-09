5cfece75d64360c5943359fd271a9c63aa406946
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-09 12:16:03 +0800

    feat: implement menu category service

--- src/main/java/id/ac/ui/cs/advprog/mewingmenu/menu/service/MenuCategoryImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/mewingmenu/menu/service/MenuCategoryImpl.java
@@ -18,23 +18,37 @@ public class MenuCategoryImpl implements MenuCategoryService {
 
     @Override
     public List<MenuCategory> getAllMenuCategories() {
+        return menuCategoryRepository.findAll();
     }
 
     @Override
     public Optional<MenuCategory> getMenuCategoryById(UUID id) {
+        return menuCategoryRepository.findById(id);
     }
 
     @Override
     public MenuCategory createMenuCategory(MenuCategory menuCategory) {
+        return menuCategoryRepository.save(menuCategory);
     }
 
     @Override
     public Optional<MenuCategory> updateMenuCategory(UUID id, MenuCategory menuCategory) {
-
+        return menuCategoryRepository.findById(id)
+                .map(existingMenuCategory -> {
+                    existingMenuCategory.setName(menuCategory.getName());
+                    existingMenuCategory.setDescription(menuCategory.getDescription());
+                    return menuCategoryRepository.save(existingMenuCategory);
+                });
     }
 
     @Override
     public void deleteMenuCategory(UUID id) {
-
+        // Check if theres any menu in this category
+        List<Menu> menus = menuCategoryRepository.findById(id).get().getMenus();
+        if (menus.isEmpty()) {
+            menuCategoryRepository.deleteById(id);
+        } else {
+            throw new IllegalStateException("Cannot delete category with existing menus");
+        }
     }
 }
