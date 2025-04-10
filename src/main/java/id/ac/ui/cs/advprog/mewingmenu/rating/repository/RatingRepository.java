60c6daa710efa288aaea715205d1c43c07d1e5fd
rafliesa <rflisaa@gmail.com>
2025-04-10 13:48:26 +0700

    feat: add more repository method/function

--- src/main/java/id/ac/ui/cs/advprog/mewingmenu/rating/repository/RatingRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/mewingmenu/rating/repository/RatingRepository.java
@@ -1,6 +1,7 @@
 package id.ac.ui.cs.advprog.mewingmenu.rating.repository;
 
 import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,7 @@ import id.ac.ui.cs.advprog.mewingmenu.menu.model.Menu;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, String> {
+    List<Rating> findAllByMenu(Menu menu);
+    Optional<Rating> findByUserId(String userId);
+    Optional<Rating> findByUserIdAndMenu(String userId, Menu menu);
 }
