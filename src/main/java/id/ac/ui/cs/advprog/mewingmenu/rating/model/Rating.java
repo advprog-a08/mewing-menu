10aa37b85577fbcb513573596b2f3ff1d8bb34e6
rafliesa <rflisaa@gmail.com>
2025-04-10 10:49:25 +0700

    feat: implement rating model

--- src/main/java/id/ac/ui/cs/advprog/mewingmenu/rating/model/Rating.java
+++ src/main/java/id/ac/ui/cs/advprog/mewingmenu/rating/model/Rating.java
@@ -1,15 +1,55 @@
 package id.ac.ui.cs.advprog.mewingmenu.rating.model;
-
 import java.time.LocalDateTime;
 
 import id.ac.ui.cs.advprog.mewingmenu.menu.model.Menu;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.hibernate.annotations.GenericGenerator;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Rating {
+    @Id
+    @GeneratedValue(generator = "uuid4")
+    @GenericGenerator(name = "uuid4",  strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(columnDefinition = "VARCHAR(36)")
     private String id;
+
+    @NotNull(message = "Menu can't be empty")
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "menu_id", referencedColumnName = "id", nullable = false)
     private Menu menu;
+
+    @NotBlank(message = "User Id can't be empty")
+    @Column(columnDefinition = "VARCHAR(36)")
     private String userId;
+
+    @NotNull(message = "Rating can't be empty")
+    @Min(value=1, message= "Rating must be at least 1")
+    @Max(value=5, message = "Rating must be 5 or lower")
     private Integer rating;
+
+    @Size(max=255, message = "Review can't exceed 255 characters")
+    @Column(nullable = false, length = 255)
     private String review;
+
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
