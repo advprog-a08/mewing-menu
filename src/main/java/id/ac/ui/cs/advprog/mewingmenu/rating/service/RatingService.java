b829f38af826c6e27a8e871699d7ad95094c90d4
rafliesa <rflisaa@gmail.com>
2025-04-10 15:14:40 +0700

    feat: make an interface for RatingService

--- src/main/java/id/ac/ui/cs/advprog/mewingmenu/rating/service/RatingService.java
+++ src/main/java/id/ac/ui/cs/advprog/mewingmenu/rating/service/RatingService.java
@@ -1,4 +1,15 @@
 package id.ac.ui.cs.advprog.mewingmenu.rating.service;
 
+import id.ac.ui.cs.advprog.mewingmenu.menu.model.Menu;
+import id.ac.ui.cs.advprog.mewingmenu.rating.model.Rating;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface RatingService {
+    Rating addRating(Rating rating);
+    Optional<Rating> findById(String ratingId);
+    List<Rating> getAllRatingsByMenu(Menu menu);
+    void deleteRatingById(String ratingId, String userId);
+    Rating updateRating(String ratingId, Rating updatedRating, String userId);
 }
