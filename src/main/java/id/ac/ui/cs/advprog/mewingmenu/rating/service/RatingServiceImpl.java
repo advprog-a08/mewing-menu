6cdc76e3d9ab9e46620708c17ed3f2e9615c4380
rafliesa <rflisaa@gmail.com>
2025-05-13 16:13:43 +0700

    [GREEN] userid -> sessionid

--- src/main/java/id/ac/ui/cs/advprog/mewingmenu/rating/service/RatingServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/mewingmenu/rating/service/RatingServiceImpl.java
@@ -21,7 +21,7 @@ public class RatingServiceImpl implements RatingService {
 
     @Override
     public Rating addRating(Rating rating) {
-        Optional<Rating> existingRating = ratingRepository.findByUserIdAndMenu(rating.getUserId(), rating.getMenu());
+        Optional<Rating> existingRating = ratingRepository.findBySessionIdAndMenu(rating.getSessionId(), rating.getMenu());
         if (existingRating.isPresent()) {
             throw new IllegalStateException("User has already reviewed this menu");
         }
@@ -39,14 +39,14 @@ public class RatingServiceImpl implements RatingService {
     }
 
     @Override
-    public void deleteRatingById(String ratingId, String userId) {
+    public void deleteRatingById(String ratingId, String sessionid) {
         Optional<Rating> ratingOpt = ratingRepository.findById(ratingId);
         if (ratingOpt.isEmpty()) {
             throw new IllegalStateException("Rating not found");
         }
 
         Rating rating = ratingOpt.get();
-        if (!rating.getUserId().equals(userId)) {
+        if (!rating.getSessionId().equals(sessionid)) {
             throw new SecurityException("User not authorized to delete this rating");
         }
 
@@ -54,7 +54,7 @@ public class RatingServiceImpl implements RatingService {
     }
 
     @Override
-    public Rating updateRating(String ratingId, Rating updatedRating, String userId) {
+    public Rating updateRating(String ratingId, Rating updatedRating, String sessionId) {
         Optional<Rating> ratingOpt = ratingRepository.findById(ratingId);
         if (ratingOpt.isEmpty()) {
             throw new IllegalStateException("Rating not found");
@@ -62,7 +62,7 @@ public class RatingServiceImpl implements RatingService {
 
         Rating existingRating = ratingOpt.get();
 
-        if (!existingRating.getUserId().equals(userId)) {
+        if (!existingRating.getSessionId().equals(sessionId)) {
             throw new IllegalStateException("You are not allowed to update this rating");
         }
 
